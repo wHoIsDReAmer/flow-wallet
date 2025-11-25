597ab77de7cf5d824f3ff9444619a53949997e34
wHoIsDReAmer <lcw060403@gmail.com>
2025-11-25 21:34:34 +0900

    feat: implement local signer

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -14,34 +14,52 @@ pub struct Wallet<T: Signer> {
     pub chain: String,
 }
 
+impl<T: Signer> Wallet<T> {
+    pub fn new(signer: T, chain: String) -> Self {
+        Self { signer, chain }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use async_trait::async_trait;
+    use k256::ecdsa::{Signature, VerifyingKey, signature::DigestVerifier};
+    use sha2::{Digest, Sha256};
 
+    use crate::wallet::signer::local::LocalSigner;
     use crate::wallet::{Signer, Wallet};
-
-    struct NoopSigner {}
-
-    #[async_trait]
-    impl Signer for NoopSigner {
-        async fn sign(&self, _message: &[u8]) -> Result<Vec<u8>, ()> {
-            todo!()
-        }
-
-        fn public_key(&self) -> Vec<u8> {
-            todo!()
-        }
-    }
 
     #[tokio::test]
     async fn test_sign() {
-        let signer = NoopSigner {};
-        let foo_wallet = Wallet {
-            signer,
-            chain: "FOO20".into(),
-        };
+        // 0x01... is a valid small scalar on secp256k1 for testing.
+        let secret = [1u8; 32];
+        let signer = LocalSigner::from_bytes(secret).expect("valid test key");
+        let foo_wallet = Wallet::new(signer, "FOO20".into());
 
         let message = b"foobar";
-        foo_wallet.signer.sign(message).await.ok();
+        let sig_bytes = foo_wallet.signer.sign(message).await.expect("signs");
+
+        // Verify signature using the public key the wallet exposes.
+        let vk_bytes = foo_wallet.signer.public_key();
+        let verifying_key = VerifyingKey::from_sec1_bytes(&vk_bytes).expect("valid pk");
+        let sig = Signature::from_der(&sig_bytes).expect("der sig");
+        let digest = Sha256::new().chain_update(message);
+        verifying_key
+            .verify_digest(digest, &sig)
+            .expect("signature should verify");
+    }
+
+    #[tokio::test]
+    async fn test_public_key_format() {
+        let secret = [2u8; 32];
+        let signer = LocalSigner::from_bytes(secret).expect("valid test key");
+
+        let pk = signer.public_key();
+        assert_eq!(
+            pk.len(),
+            33,
+            "compressed SEC1 public key should be 33 bytes"
+        );
+
+        VerifyingKey::from_sec1_bytes(&pk).expect("public key must parse");
     }
 }
--- src/wallet/signer/local.rs
+++ src/wallet/signer/local.rs
@@ -1,16 +1,40 @@
 use async_trait::async_trait;
+use k256::ecdsa::{Signature, SigningKey, VerifyingKey, signature::DigestSigner};
+use sha2::{Digest, Sha256};
 
 use crate::wallet::Signer;
 
-// TODO: implments LocalSigner
-pub struct LocalSigner;
+/// Local software signer backed by an in-memory secp256k1 private key.
+pub struct LocalSigner {
+    signing_key: SigningKey,
+}
+
+impl LocalSigner {
+    /// Create a signer from a 32-byte secp256k1 secret scalar.
+    pub fn from_bytes(secret_key: [u8; 32]) -> Result<Self, k256::ecdsa::Error> {
+        let signing_key = SigningKey::from_bytes(&secret_key.into())?;
+        Ok(Self { signing_key })
+    }
+
+    /// Return the compressed public key (33 bytes, SEC1).
+    fn compressed_public_key(&self) -> VerifyingKey {
+        self.signing_key.verifying_key().to_owned()
+    }
+}
 
 #[async_trait]
 impl Signer for LocalSigner {
-    async fn sign(&self, _message: &[u8]) -> Result<Vec<u8>, ()> {
-        todo!()
+    async fn sign(&self, message: &[u8]) -> Result<Vec<u8>, ()> {
+        // Hash the message to 32 bytes; required size for secp256k1 signing.
+        let digest = Sha256::new().chain_update(message);
+        let signature: Signature = self.signing_key.sign_digest(digest);
+        Ok(signature.to_der().as_bytes().to_vec())
     }
+
     fn public_key(&self) -> Vec<u8> {
-        todo!()
+        self.compressed_public_key()
+            .to_encoded_point(true)
+            .as_bytes()
+            .to_vec()
     }
 }
